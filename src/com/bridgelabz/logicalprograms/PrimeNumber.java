fedc3b2ad5566af9a0b8838cee71bda7215f28d1
shiwani1801 <113423996+shiwani1801@users.noreply.github.com>
2023-01-10 01:52:01 -0800

    Added: Prime Number program

--- src/com/bridgelabz/logicalprograms/PrimeNumber.java
+++ src/com/bridgelabz/logicalprograms/PrimeNumber.java
@@ -4,7 +4,7 @@ import java.util.Scanner;
 
 public class PrimeNumber {
     public static void main(String[] args) {
-        int i=2;
+        int i = 2;
         System.out.println("enter nos.");
         Scanner sc=new Scanner(System.in);
         int num=sc.nextInt();
